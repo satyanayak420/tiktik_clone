8a6b3b7762ad6edfcc5a1ba690a1d3d6e8ff24de
Satya <nayak.satya@mahindra.com>
2022-11-11 22:26:16 +0530

    typescript nextconfig

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  typescript: {
+    ignoreBuildErrors: true,
+  },
   images: {
     domains: ["scontent.fbom12-1.fna.fbcdn.net", "lh3.googleusercontent.com"],
   },
