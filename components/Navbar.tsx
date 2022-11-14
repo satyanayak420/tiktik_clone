7cc5983a439b61dc30f5f0c448a0772a0975b8dd
Satya <nayak.satya@mahindra.com>
2022-11-14 21:06:38 +0530

    'minor_commit'

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -50,8 +50,9 @@ const Navbar = () => {
     <div
       className={`w-full relative flex justify-between items-center border-b-2 border-gray-200 py-2 px-4 items-center`}
     >
-      <FaVideo className={`${showLogo}`} />
-
+      <Link href="/">
+        <FaVideo className={`${showLogo}`} />
+      </Link>
       <Link href="/">
         <div
           className={`w-[100px] md:w-[129px] md:h-[30px] h-[38px] ${showLogo1}`}
